382d157bbca66d7b8243f4fed3285648aa315517
Volker Otto <l4ci@Volkers-MacBook-Pro.local>
2013-09-11 11:03:34 +0200

    Many Improvments
    added jquery 2.0.3
    added some animation (dropdown menus, footer)
    removed subreddit styles via removing the link element
    changed colors to blue
    changed bold titles back to normal
    started with responsive media queries

---
+++ js/main.js
@@ -0,0 +1,35 @@
+/*
+ LOAD CSS AFTER PAGE LOAD
+var style = document.createElement('link');
+style.rel = 'stylesheet';
+style.type = 'text/css';
+style.href = chrome.extension.getURL('main.css');
+(document.head||document.documentElement).appendChild(style);
+*/
+
+// Insert meta viewport tag
+var $meta = $('meta[name="viewport"]');
+$meta.attr('content','width=device-width, initial-scale=1.0');
+
+
+// Disable subreddit stylesheet
+var $userstyle = $('link[rel="stylesheet"][title="applied_subreddit_stylesheet"]');
+$userstyle.attr('disabled', 'disabled');
+
+// Disable all dropdown menu onclick event and replace with animation
+var $menu = $('.dropdown');
+$menu.removeAttr('onclick').prop('onclick', null);
+$menu.click(function() {
+  $(this).nextAll('.drop-choices').slideToggle();
+});
+
+// Replace Footer Menu with Link + Animation
+var $footer = $('.footer-parent');
+$('.footer').hide();
+$('.bottommenu').hide();
+$footer.prepend('<a id="show_footer">Show Footer Navigation</a>');
+$('#show_footer').click(function(){
+	$(this).hide();
+	$('.footer').slideDown();
+	$('.bottommenu').fadeIn();
+});
--- main.js
+++
@@ -1,5 +0,0 @@
-var style = document.createElement('link');
-style.rel = 'stylesheet';
-style.type = 'text/css';
-style.href = chrome.extension.getURL('main.css');
-(document.head||document.documentElement).appendChild(style);
